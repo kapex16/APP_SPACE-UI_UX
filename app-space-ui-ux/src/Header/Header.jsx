9a43f9b6c0b65b59f99aad659bd2f24b0c943c91
Jorge Andres Florentin Escurra <salesapex.19@gmail.com>
2021-08-22 21:35:36 +0200

    Menu-22-08-2021

--- app-space-ui-ux/src/Header/Header.jsx
+++ app-space-ui-ux/src/Header/Header.jsx
@@ -1,8 +1,56 @@
+import {
+  AppBar,
+  Typography,
+  Toolbar,
+  IconButton,
+  
+} from "@material-ui/core";
+import { AccountCircle } from "@material-ui/icons";
+import Logo from "../asset/LOGO.png";
+import useSteyles from '../Header/Style'
 
 
 
+const MenuSpaceBar = () => {
+  const classes = useSteyles();
 
-export default function MenuSpaceBar(){
-
-    
-}
+  return (
+    <div>
+      <AppBar position="fixed" className={classes.LogoConatiner}>
+        <Toolbar>
+            <di >
+              <img 
+              className={classes.LogoStyle}
+              src={Logo} 
+              alt="Logo" />
+            </di>
+            <div className={classes.ContainerMenuRight}>
+            <div className={classes.StyleBoxContainerAccount}>
+             <div className={classes.StyleBoxContainerTitle}>
+            <Typography
+            variant="body1"
+            className={classes.StyleCapsule}
+            >Capsula</Typography>
+            <Typography
+            variant="body1"
+            className={classes.StyleParadox}
+            >Paradoja</Typography>
+            <Typography
+            variant="body1"
+            className={classes.StyleHistory}
+            >Historia</Typography>
+            </div>
+            <IconButton 
+            
+            >
+              <AccountCircle className={classes.AccountStyle}/>
+            </IconButton>
+            </div>
+            </div>
+          
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
+export default MenuSpaceBar;
