37c51824fd2c7f6807b10e025aa0952a20ca85df
Jorge Andres Florentin Escurra <salesapex.19@gmail.com>
2021-08-26 19:43:23 +0200

    Cambios-Estilos

--- app-space-ui-ux/src/Header/Header.jsx
+++ app-space-ui-ux/src/Header/Header.jsx
@@ -1,56 +1,39 @@
-import {
-  AppBar,
-  Typography,
-  Toolbar,
-  IconButton,
-  
-} from "@material-ui/core";
+import { AppBar, Typography, Toolbar, IconButton } from "@material-ui/core";
 import { AccountCircle } from "@material-ui/icons";
 import Logo from "../asset/LOGO.png";
-import useSteyles from '../Header/Style'
-
-
+import useSteyles from "../Header/Style";
 
 const MenuSpaceBar = () => {
   const classes = useSteyles();
 
   return (
-    <div>
+    
       <AppBar position="fixed" className={classes.LogoConatiner}>
         <Toolbar>
-            <di >
-              <img 
-              className={classes.LogoStyle}
-              src={Logo} 
-              alt="Logo" />
-            </di>
-            <div className={classes.ContainerMenuRight}>
+          <di>
+            <img className={classes.LogoStyle} src={Logo} alt="Logo" />
+          </di>
+          <div className={classes.ContainerMenuRight}>
             <div className={classes.StyleBoxContainerAccount}>
-             <div className={classes.StyleBoxContainerTitle}>
-            <Typography
-            variant="body1"
-            className={classes.StyleCapsule}
-            >Capsula</Typography>
-            <Typography
-            variant="body1"
-            className={classes.StyleParadox}
-            >Paradoja</Typography>
-            <Typography
-            variant="body1"
-            className={classes.StyleHistory}
-            >Historia</Typography>
+              <div className={classes.StyleBoxContainerTitle}>
+                <Typography variant="body1" className={classes.StyleCapsule}>
+                  Capsula
+                </Typography>
+                <Typography variant="body1" className={classes.StyleParadox}>
+                  Paradoja
+                </Typography>
+                <Typography variant="body1" className={classes.StyleHistory}>
+                  Historia
+                </Typography>
+              </div>
+              <IconButton>
+                <AccountCircle className={classes.AccountStyle} />
+              </IconButton>
             </div>
-            <IconButton 
-            
-            >
-              <AccountCircle className={classes.AccountStyle}/>
-            </IconButton>
-            </div>
-            </div>
-          
+          </div>
         </Toolbar>
       </AppBar>
-    </div>
+    
   );
 };
 export default MenuSpaceBar;
